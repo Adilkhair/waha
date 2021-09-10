6205d928821af62e63d52832b646caf0c6560bb2
Adil <adilindea@gmail.com>
2021-09-10 13:17:51 -0700

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2021   أحفاد خاطر للانشطة المتعددة </b> كل الحقوق محفظة.
+		</div>
+	</div>
---
+++ includes/myaccount-sidebar.php
@@ -0,0 +1,20 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">Your Checkout Progress</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">حسابي</a></li>
+					<li><a href="bill-ship-addresses.php">عنوان الدفع</a></li>
+					<li><a href="order-history.php">الطلبات السابقة</a></li>
+					<li><a href="pending-orders.php">طلبات الدفع المعلقة</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
